0fc9df5e2a0a36f4d0260a6fa8ad135789685002
Wu <superbatironmans5@gmail.com>
2015-04-24 00:00:06 +0800

    Finish Search Page

--- index.ios.js
+++ index.ios.js
@@ -1,23 +1,40 @@
 'use strict'
 
 var React = require('react-native');
-
+var SearchPage = require('./SearchPage');
 var {
-  AppRegistry
+  AppRegistry,
+  NavigatorIOS,
+  Text
 } = React;
 
 var styles = React.StyleSheet.create({
   text: {
     color: 'black',
-    backgroundColor: 'white',
-    fontSize: 30,
+    backgroundColor: 'yellow',
+    fontSize: 40,
     margin: 80
+  },
+  container: {
+    flex: 1
   }
 });
-
+class HelloWorld extends React.Component {
+  render() {
+    return(
+    <Text style={styles.text}>Hello World (Again)</Text>
+    )
+  }
+}
 class Test extends React.Component {
   render() {
-    return React.createElement(React.Text, {style: styles.text}, "Hello World!");
+    return (
+     <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Property Finder',
+          component: SearchPage,
+        }}/>)
   }
 }
 
